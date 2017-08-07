9bd3820069c40cc655c54be3eb28e5065b25858a
Taylor <tbaustin1992@gmail.com>
2017-08-07 13:37:00 -0500

    finished 'handling webhook data section'

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,25 +10,45 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks so much for voting');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = req.body.map(({ url, email }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
-      if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        };
-      }
+    console.log('recieved data from webhook');
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
-      console.log(events);
-    });
+    _.chain(req.body)
+      .map(({ url, email }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact() //removes unidentifed objects
+      .uniqBy('email', 'surveyId') //sorts out duplicates by values
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value(); //returns value from full chain block
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -38,7 +58,7 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email })),
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
---
+++ sendgrid_webhook.js
@@ -0,0 +1,7 @@
+var localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: 'jjj222jj33jj33ii444ss221199sd' }, function(
+  err,
+  tunnel
+) {
+  console.log('LT running');
+});
